174388ec10b7bff7edd8b7b75e8b39c4af098b26
palina-mrk <khudziakova@tut.by>
2025-10-30 12:53:43 +0300

    actions-card

---
+++ coffee-front/src/components/cards/ActionCard.vue
@@ -0,0 +1,98 @@
+<script setup>
+const props = defineProps(["actionInfo"]);
+</script>
+
+<template>
+  <div class="action-card">
+    <p class="action-card__slogan">{{ action.slogan }}</p>
+    <span
+      class="action-card__until-str"
+      >Срок акции: до&nbsp;{{ action.until }}</span
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.catalog-card {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: end;
+  background-color: $color-black-302;
+  background-image: url("../../images/personal/actions-card-desktop.png");
+  padding: 39px 20px;
+  border-radius: 20px;
+  gap: 88px;
+  
+  @include vp-laptop {
+    background-image: url("../../images/personal/actions-card-laptop.png");
+    padding: 24px 15px;
+    border-radius: 14px;
+    gap: 62px;
+  }
+
+  @include vp-tablet {
+    background-image: url("../../images/personal/actions-card-tablet.png");
+    padding: 149px 40px;
+    border-radius: 16px;
+    gap: 70px;
+  }
+
+  @include vp-mobile {
+    background-image: url("../../images/personal/actions-card-mobile.png");
+    padding: 60px 30px;
+    border-radius: 10px;
+    gap: 30px;
+  }
+
+  &__slogan {
+    font-family: $ff-gilroy sans-serif;
+    color: $color-white;
+    margin: 0;
+    padding: 0;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 36px;
+
+    @include vp-laptop {
+      font-size: 20px;
+      line-height: 26px;
+    }
+
+    @include vp-tablet {
+      font-size: 24px;
+      line-height: 29px;
+    }
+
+    @include vp-mobile {
+      font-size: 14px;
+      line-height: 17px;
+    }
+  }
+
+  &__until-str {
+    font-family: $ff-gilroy sans-serif;
+    color: $color-white;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 24px;
+
+    @include vp-laptop {
+      font-size: 14px;
+      line-height: 17px;
+    }
+
+    @include vp-tablet {
+      font-size: 16px;
+      line-height: 19px;
+    }
+
+    @include vp-mobile {
+      font-size: 14px;
+      line-height: 16px;
+    }
+  }
+}
+</style>
--- coffee-front/src/components/globals/PersonalBody.vue
+++ coffee-front/src/components/globals/PersonalBody.vue
@@ -2,25 +2,32 @@
 import BgPersonal from "../backgrounds/BgPersonal.vue";
 
 import CustomBreadcrumbs from "../navigation/CustomBreadcrumbs.vue";
-import CustomToggle from "../toggles/CustomToggle.vue";
+import ActionCard from "../cards/ActionCard.vue";
 
 import { reactive, ref } from "vue";
-const toggleValues = reactive([
+const actionsInfo = reactive([
   {
-    label: "Контакты",
-    value: "contacts",
+    slogan: "Купи 3 пачки кофе и получи 4-ую в подарок!",
+    until: "31.11.2025",
   },
   {
-    label: "Наши магазины",
-    value: "our-shops",
+    slogan: "Купи 3 пачки кофе и получи 4-ую в подарок!",
+    until: "31.11.2025",
+  },
+  {
+    slogan: "Купи 3 пачки кофе и получи 4-ую в подарок!",
+    until: "31.11.2025",
+  },
+  {
+    slogan: "Купи 3 пачки кофе и получи 4-ую в подарок!",
+    until: "31.11.2025",
   },
 ]);
-const selectedPageVariant = ref("contacts");
 </script>
 
 <template>
   <main>
-    <section class="personal">
+    <section class="personal" style="min-height: 2000px;">
       <bg-personal></bg-personal>
 
       <div class="container">
@@ -28,24 +35,16 @@ const selectedPageVariant = ref("contacts");
 
         <custom-breadcrumbs class="personal__breadcrumbs"></custom-breadcrumbs>
 
-        <custom-toggle
-          class="contacts__toggle"
-          :initialValues="toggleValues"
-          :toggleName="contacts"
-          :selected="selectedPageVariant"
-          @toggle-value="selectedPageVariant = $event"
-        ></custom-toggle>
-
-        
         <div class="personal__forms-wrapper">
-          <cart-card class="personal__user-card"></cart-card>
-          <delivery-form class="personal__orders-card"></delivery-form>
         </div>
 
         <div class="personal__actions-wrapper">
           <h2 class="personal__actions-title">Персональные акции</h2>
           <ul class="personal__actions-list">
-            <li class="personal__actions-item"></li>
+            <li class="personal__actions-item"
+            v-for="actionInfo in actionsInfo">
+            <action-card :actionInfo="actionInfo"></action-card>
+          </li>
           </ul>
         </div>
       </div>
@@ -58,11 +57,9 @@ const selectedPageVariant = ref("contacts");
   position: relative;
   width: 100%;
   height: 100%;
-  padding: 202px 0 220px;
+  padding: 202px 0 100px;
   margin: 0;
-  color: $color-raising-black;
   font-family: $ff-gilroy sans-serif;
-  font-weight: 500;
   overflow: hidden;
 
   @include vp-laptop {
@@ -93,276 +90,72 @@ const selectedPageVariant = ref("contacts");
     }
   }
 
-  &__toggle {
-    height: 60px;
-    width: 100%;
-    margin: 0 0 90px;
-
-    @include vp-laptop {
-      height: 42px;
-      margin: 0 0 64px;
-    }
-
-    @include vp-tablet {
-      height: 160px;
-      margin: 0 0 60px;
-    }
-
-    @include vp-mobile {
-      height: 90px;
-      margin: 0 0 40px;
-    }
+  &__actions-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 40px;
   }
 
-  &__wrapper {
-    width: 100%;
-    justify-content: space-between;
-    display: flex;
+  &__actions-title {
+    font-family: $ff-gilroy sans-serif;
+    color: $color-raising-black;
+    margin: 0;
+    padding: 0;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 36px;
+
+    @include vp-laptop {
+      font-size: 20px;
+      line-height: 26px;
+    }
 
     @include vp-tablet {
-      flex-direction: column;
-      gap: 60px;
+      font-size: 24px;
+      line-height: 29px;
     }
 
     @include vp-mobile {
-      gap: 40px;
+      font-size: 14px;
+      line-height: 17px;
     }
   }
 
   &__list {
     display: flex;
-    flex-direction: column;
-    align-items: start;
+    overflow: hidden;
     margin: 0;
     padding: 0;
     list-style-type: none;
-    gap: 50px;
+    gap: 20px;
+    height: 449px;
 
     @include vp-laptop {
       gap: 45px;
     }
 
     @include vp-tablet {
-      gap: 20px;
-      min-height: 227px;
-      max-height: 321px;
-      flex-wrap: wrap;
     }
 
     @include vp-mobile {
-      gap: 30px;
-      min-height: unset;
-      max-height: unset;
     }
   }
 
   &__item {
+    display: flex;
+    width: 400px;
     padding: 0;
     margin: 0;
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
-    align-items: start;
 
     @include vp-laptop {
-      gap: 20px;
     }
 
     @include vp-tablet {
-      gap: 11px;
-      width: 340px;
     }
 
     @include vp-mobile {
-      gap: 20px;
     }
-  }
-
-  &__item:nth-last-child(2) {
-    @include vp-tablet {
-      order: 2;
-    }
-
-    @include vp-mobile {
-      order: unset;
-    }
-  }
-
-  &__item-title {
-    margin: 0;
-    padding: 0;
-    font-weight: 700;
-    font-size: 30px;
-    line-height: 36px;
-    font-family: $ff-gilroy sans-serif;
-    color: $color-black;
-
-    @include vp-laptop {
-      font-size: 22px;
-      line-height: 26px;
-    }
-
-    @include vp-tablet {
-      font-size: 20px;
-      line-height: 26px;
-      max-width: 220px;
-    }
-
-    @include vp-mobile {
-      font-size: 18px;
-      line-height: 22px;
-    }
-  }
-
-  &__item-content {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-
-    @include vp-laptop {
-      gap: 7px;
-    }
-
-    @include vp-tablet {
-      gap: 3px;
-    }
-
-    @include vp-mobile {
-      gap: 20px;
-      flex-direction: column-reverse;
-    }
-  }
-
-  &__item-sublist {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-  }
-
-  &__item-paragraph,
-  &__item-link,
-  &__item-line {
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
-    font-family: $ff-gilroy sans-serif;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 26px;
-    color: $color-raising-black;
-    max-width: 540px;
-
-    @include vp-laptop {
-      font-size: 14px;
-      line-height: 19px;
-      max-width: 375px;
-    }
-
-    @include vp-tablet {
-      font-size: 20px;
-      line-height: 26px;
-      max-width: 340px;
-    }
-
-    @include vp-mobile {
-      font-size: 14px;
-      line-height: 18px;
-      max-width: unset;
-    }
-  }
-
-  &__item-link {
-    width: fit-content;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-
-    &:hover .contacts__item-line {
-      position: relative;
-    }
-
-    &:hover .contacts__item-line::after {
-      background-color: $color-ucla-gold;
-      bottom: -2px;
-      height: 2px;
-      left: 0;
-      right: 0;
-      position: absolute;
-      content: "";
-
-      @include vp-laptop {
-        height: 1px;
-        bottom: -1px;
-      }
-
-      @include vp-tablet {
-        bottom: -1px;
-      }
-    }
-
-    &:hover .contacts__icon-wrapper {
-      @include vp-mobile {
-        border-color: $color-raising-black;
-        color: $color-raising-black;
-      }
-    }
-  }
-
-  &__icon-wrapper {
-    display: none;
-
-    @include vp-mobile {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-color: $color-ucla-gold;
-      color: $color-white;
-      border-radius: 50%;
-      border-width: 1px;
-      border-style: solid;
-      width: 22px;
-      height: 22px;
-    }
-  }
-
-  &__icon-svg {
-    @include vp-mobile {
-      width: 12px;
-      height: 12px;
-    }
-  }
-
-  &__map-wrapper {
-    width: 960px;
-    height: 526px;
-    border-radius: 5px;
-    overflow: hidden;
-    border-width: 1px;
-    border-color: $color-quick-silver-a4;
-    border-style: solid;
-    display: flex;
-
-    @include vp-laptop {
-      width: 680px;
-      height: 372px;
-    }
-
-    @include vp-tablet {
-      width: 100%;
-    }
-
-    @include vp-mobile {
-      width: 340px;
-      height: 288px;
-    }
-  }
-
-  &__map-image {
-    width: 100%;
-    height: 100%;
   }
 }
 </style>
