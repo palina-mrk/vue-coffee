702198f262adc94d621069baeb8b97f902c89270
palina-mrk <khudziakova@tut.by>
2025-11-03 20:33:39 +0300

    personal-is-finished

--- coffee-front/src/stores/user-info.js
+++ coffee-front/src/stores/user-info.js
@@ -8,9 +8,29 @@ export const useUserInfoStore = defineStore("user-info", () => {
   );
 
   if(userInfo.name == undefined || userInfo.name.length == 0)
-  
+    userInfo.name = 'Иван Иванов';
+
+  if(userInfo.email == undefined || userInfo.email.length == 0)
+    userInfo.email = 'ivan.ivanov@gmail.com';
+
+  if(userInfo.tel == undefined || userInfo.tel.length == 0)
+    userInfo.tel = '+ 7 (909) 909 99 99';
+
+  if(userInfo.password == undefined || userInfo.password.length == 0)
+    userInfo.password = 'password';
+
   function setUserInfo (obj) {
-    Object.keys(obj).forEach(key => userInfo[key] = obj[key])
+    if(obj.name && obj.name.length > 0)
+      userInfo.name = obj.name;
+
+    if(obj.email && obj.email.length > 0)
+      userInfo.email = obj.email;
+
+    if(obj.password && obj.password.length > 0)
+      userInfo.password = obj.password;
+
+    if(obj.tel && obj.tel.length > 0)
+      userInfo.tel = obj.tel;
   }
 
   watch(userInfo, () => {
